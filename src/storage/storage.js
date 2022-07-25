07113fbc24f7c0c26a957028ac046c10bc01da6f
Ali Hassaine <ali.hs@outlook.fr>
2022-07-25 15:29:29 +0000

    Add storage methods.
    Note: The code contains duplication in the form of
    sandwich code, this needs refactoring.

--- src/storage/storage.js
+++ src/storage/storage.js
@@ -1,26 +1,46 @@
+import { STORAGE_KEY } from "./key";
+import { serialize, deserialize } from '../helpers/serialize';
 
-function addTodoToStorage(projectIndex, todo) {
 
+function addTodoToStorage(projectIndex, todoIndex, todoObj) {
+    //1. Get the storage data.
+    let data = deserialize(localStorage.getItem(STORAGE_KEY));
+
+    //2. Operation
+    data[projectIndex].todos[todoIndex] = todoObj;
+
+    //3. Insert the data back.
+    let stringified = serialize(data);
+    localStorage.setItem(STORAGE_KEY, stringified);
 }
 
 
-function addProjectToStorage(projectIndex) {
-
+function addProjectToStorage(projectIndex, projectObject) {
+    let data = deserialize(localStorage.getItem(STORAGE_KEY));
+    data[projectIndex] = projectObject;
+    let stringified = serialize(data);
+    localStorage.setItem(STORAGE_KEY, stringified);
 }
 
-function  updateTodoInStorage(projectIndex, todoIndex) {
-
+function  updateTodoInStorage(projectIndex, todoIndex, todoObj) {
+    addTodoToStorage(projectIndex,todoIndex, todoObj );
 }
 
-function updateProjectInStorage(projectIndex) {
-
+function updateProjectInStorage(projectIndex, projectObject) {
+    addProjectToStorage(projectIndex, projectObject);
 }
 
 function deleteTodoFromStorage(projectIndex, todoIndex) {
-
+    let data = deserialize(localStorage.getItem(STORAGE_KEY));
+    delete data[projectIndex].todos[todoIndex]
+    let stringified = serialize(data);
+    localStorage.setItem(STORAGE_KEY, stringified);
 }
 
 function deleteProjectFromStorage(projectIndex) {
-
+    let data = deserialize(localStorage.getItem(STORAGE_KEY));
+    delete data[projectIndex];
+    let stringified = serialize(data);
+    localStorage.setItem(STORAGE_KEY, stringified);
 }
 
